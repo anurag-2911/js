c19512a4b7f2fa837c103994f36ff37561eff474
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-07-16 00:53:59 +0530

    variables.

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 let js = 'hello'
 
 if (js === 'hello') {
-    alert('hellow world');
+    console.log('hellow world');
 }
+
+let ispresent = true;
+
+console.log(typeof(ispresent));
+
+console.log('string with lines \n\
+line 1\n\
+line 2 \n\ '
+)
+const firstName ='anurag';
+const lastname ='kumar'
+const namee  = `hello ${firstName} how are you ${lastname}`;
+
+console.log(namee);
