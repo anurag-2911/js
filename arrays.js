e6d3bd218b89e16c2f3037e020267f3ca44d72dc
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-07-29 23:49:53 +0530

    objects in js

--- arrays.js
+++ arrays.js
@@ -1,3 +1,9 @@
 const arr = ['one','two','three'];
 arr.push('four');
+arr.unshift('kool');
 console.log(arr);
+arr.shift();
+console.log(arr);
+arr.pop();
+console.log(arr);
+console.log(arr.indexOf('one'));
