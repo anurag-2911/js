3e93749e16452395c1806489a9554a91c2a0476c
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-07-29 15:46:35 +0530

    clean up

--- script.js
+++ script.js
@@ -23,3 +23,47 @@ line2
 line3`
 
 console.log(multi);
+class Gender {
+    constructor(){
+        this.gender = 'M';
+    }
+    printGender(){
+        console.log(this.gender);
+    }
+}
+class Person extends Gender{
+    constructor() {
+        super();
+        this.name= 'anuragKK'
+    }
+    printName(){
+        console.log(this.name);
+    }
+}
+
+const person01= new Person();
+person01.printName();
+person01.printGender();
+
+class Bundle {
+    name;
+    version;
+    printBundle = ()=>{
+        this.name = 'bun01';
+        this.version ='101';
+        console.log(' bundle details ' + this.name + ' ' + this.version);
+    }
+}
+class WindowsBundle extends Bundle {
+    metadata;
+    printMetadata(){
+        this.metadata = 'dfgj';
+        console.log('metada ' + this.metadata);
+        console.log('and '+this.name + ' : '+this.version);
+    }
+}
+const b = new Bundle();
+b.printBundle();
+
+const wb = new WindowsBundle();
+wb.printMetadata();
